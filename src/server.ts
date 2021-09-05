e55a7df7f36372eab171d25dd1471ad4466cc32c
Folusho Oladipo <10248833+folushooladipo@users.noreply.github.com>
2021-09-06 00:52:46 +0100

    Add version 0.0.1 (#5)
    * Switch from npm to yarn
    
    * Setup ESLint
    
    * Add project info to package.json
    
    * Uninstall @types/bluebird
    
    * Delete tslint.json
    
    * Uninstall TSLint
    
    * Replace the deprecated body-parser dependency
    
    * Enable the filtering of images
    
    * Improve some error messages
    
    * Complete a todo
    
    * Require an API key before filtering images (#3)
    
    * Require an API key before filtering images
    
    * Modify how validateAPIKey() works
    
    * Update info in .env.sample
    
    * Specify minimum Node.js version used by app
    
    * Prepare for project submission (#4)
    
    * Add screenshots of my Elastic Beanstalk app
    
    * Add my submission's info to the project's README
    
    * Add Postman collection for making API requests
    
    * Update README
    
    * Add info about my deployment of the UI
    
    * Update version

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,121 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, {json as parseJsonBody} from "express"
+import {config as loadEnvironmentVariables} from "dotenv"
+import {sign as signJWT} from "jsonwebtoken"
 
-(async () => {
+import {deleteLocalFiles, filterImageFromURL, validateURL} from "./util/util"
+import {validateAPIKey} from "./middleware"
 
-  // Init the Express application
-  const app = express();
+loadEnvironmentVariables()
+const SECONDS_IN_ONE_YEAR = 60 * 60 * 24 * 365;
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+(() => {
+  const app = express()
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  const port = process.env.PORT || 8082
 
-  /**************************************************************************** */
+  app.use(parseJsonBody())
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/filteredimage", validateAPIKey, async (req, res) => {
+    const {image_url: imageURL} = req.query
+    if (!imageURL) {
+      return res
+        .status(400)
+        .send("Error: an image URL was not supplied.")
+    }
+    if (typeof imageURL !== "string") {
+      return res
+        .status(422)
+        .send("Error: image URL must be a string.")
+    }
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
-})();
+    const isImageURLValid = validateURL(imageURL)
+    if (!isImageURLValid) {
+      return res
+        .status(400)
+        .send("Error: the image URL supplied is not a valid URL.")
+    }
+
+    let pathToFilteredImage: string
+    try {
+      pathToFilteredImage = await filterImageFromURL(imageURL)
+    } catch (err) {
+      console.error("Failed to filter image because of this error:", err)
+      return res
+        .status(500)
+        .send("The image at the given URL isn't accessible. Please check the URL and try again.")
+    }
+
+    res.sendFile(pathToFilteredImage, (err) => {
+      if (err) {
+        console.error("Failed to send filtered image to client because of this error:", err)
+      }
+
+      deleteLocalFiles([pathToFilteredImage])
+    })
+  })
+
+  const trustedSourceIDs = {
+    [process.env.TRUSTED_SOURCE_LOCAL]: true,
+    [process.env.TRUSTED_SOURCE_DEV]: true,
+  }
+  app.get("/apikey", (req, res) => {
+    // A more practical but complicated way to do this would be to require a
+    // JWT token in the request and then ask Metagram API if that token belongs
+    // to a known user. If it did, an API key would be issued.
+    const {sourceID} = req.query
+    if (!sourceID) {
+      return res
+        .status(400)
+        .json({
+          error: "A source ID was not supplied.",
+        })
+    }
+
+    if (typeof sourceID !== "string") {
+      return res
+        .status(422)
+        .json({
+          error: "Source ID must be a string.",
+        })
+    }
+
+    if (!trustedSourceIDs[sourceID]) {
+      return res
+        .status(403)
+        .json({
+          error: "You are not permitted to generate an API key.",
+        })
+    }
+
+    const payload = {
+      time: Date.now(),
+      randomNumber: Math.random(),
+    }
+    const secondsSinceUnixEpoch = Math.floor(Date.now() / 1000)
+    const expiresIn = secondsSinceUnixEpoch + SECONDS_IN_ONE_YEAR
+    const jwtToken = signJWT(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn,
+      }
+    )
+    const expiresInMilliseconds = expiresIn * 1000
+    res
+      .json({
+        token: jwtToken,
+        expiryDate: (new Date(expiresInMilliseconds)).toISOString(),
+      })
+  })
+
+  app.get("/", (req, res) => {
+    res.send("This is the root route and it doesn't do much. Instead, try sending a request as: GET /filteredimage?image_url=SOME_URL")
+  })
+
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server running at http://localhost:${port}`)
+    // eslint-disable-next-line no-console
+    console.log(`Press CTRL+C to stop server.`)
+  })
+})()
